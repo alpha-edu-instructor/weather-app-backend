e90ab5a952995f281949c805d8537f2b2c6b8b17
Adil Zhexenov <adilzhexenoff@gmail.com>
2025-02-04 02:11:33 +0500

    lesson 4: solve cors error

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 import express from "express";
-import "dotenv/config";
+import cors from "cors";
 import {
   getCoordsOfCity,
   getCurrentWeather,
   getForecastData
 } from "./services/weatherService.js";
+import "dotenv/config";
+
+console.log(process.env.API_KEY);
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 const PORT = 8080;
 
